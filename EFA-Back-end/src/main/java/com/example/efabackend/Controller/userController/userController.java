0ecac746c7b81fa6b0aa4ac3e8a2aa77f3b54081
yassinazouz <serjazouz@gmail.com>
2024-05-11 09:45:37 +0100

    yassin

--- EFA-Back-end/src/main/java/com/example/efabackend/Controller/userController/userController.java
+++ EFA-Back-end/src/main/java/com/example/efabackend/Controller/userController/userController.java
@@ -46,6 +46,6 @@ public class userController {
         responseList.add(new CountResponse(enseignantCount, "Enseignant"));
         return ResponseEntity.ok(responseList);
     }
-g
+
 
 }
--- EFA-Front-end/src/app/components/add-file/add-file.component.ts
+++ EFA-Front-end/src/app/components/add-file/add-file.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CoursService } from 'src/app/services/cours.service';
 
 @Component({
   selector: 'app-add-file',
@@ -13,7 +14,7 @@ export class AddFileComponent {
   imgFile: string;
   urlFile: string;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private coursService: CoursService) {}
 
   addFile(): void {
     const formData = {
@@ -23,22 +24,20 @@ export class AddFileComponent {
       urlFile: this.urlFile
     };
 
-    this.http.post('http://localhost:8081/api/files/addFile', formData)
-      .subscribe(
-        response => {
-          console.log('File added successfully:', response);
-          // Reset form fields after successful submission if needed
-          this.id = '';
-          this.nameFile = '';
-          this.imgFile = '';
-          this.urlFile = '';
-          
-        },
-        error => {
-          console.error('Error adding file:', error);
-          // Handle error message display or other actions
-        }
-      );
+    this.coursService.addFile(formData).subscribe(
+      response => {
+        console.log('File added successfully:', response);
+        // Reset form fields after successful submission if needed
+        this.id = '';
+        this.nameFile = '';
+        this.imgFile = '';
+        this.urlFile = '';
+      },
+      error => {
+        console.error('Error adding file:', error);
+        // Handle error message display or other actions
+      }
+    );
   }
 
   
